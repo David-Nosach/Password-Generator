8c7cf01a5e679fb1797702cac727a0124895de89
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2024-03-07 16:22:04 -0800

    length of characters done

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
 // Assignment code here
+function generatePassword() {
+  while (true) {
+    var lengthChar = prompt("How many charcters would you like? (8-128)");
+    if (lengthChar <= 128 && lengthChar >= 8) {
+      break;
+    }
+    alert("Password length must be 8 to 128 characters long!");
+  }
 
+  // var lengthChar = prompt("Length of Password (8-128 characters)");
+
+  // if (lengthChar < 8 || lengthChar > 128) {
+  //   alert("make length be between 8 - 128");
+  // } else {
+  //   return lengthChar;
+  // }
+
+  var lowerChar = confirm("Do you want lowercase characters?");
+  var upperChar = confirm("Do you want uppercase characters?");
+  var numberChar = confirm("Do you want numbers?");
+  var specialChar = confirm("Do you want special characters?");
+
+  return lengthChar + lowerChar + upperChar + numberChar + specialChar;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +33,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
